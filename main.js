ae06ba7984d855a43b6b1d923a909b959cca1fcb
Afpa <alex.nesvit2000@gmail.com>
2023-12-20 14:11:43 +0100

    main.js swiper correct

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
