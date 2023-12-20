19a37f2d82b829fcc219ecd2d8d0f4c25f280d3c
Afpa <alex.nesvit2000@gmail.com>
2023-12-20 14:46:18 +0100

    css buttons ok mais ne voir pas)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 2,
     spaceBetween: 20,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
     },
   });
