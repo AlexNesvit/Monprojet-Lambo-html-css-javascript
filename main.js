345ffb5989da0afb403e6e8d7ee7603e547cf24f
Afpa <alex.nesvit2000@gmail.com>
2023-12-21 10:42:41 +0100

    map js+css ok

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-left',
     },
   });
+
+  let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
